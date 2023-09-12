fb4f875325f0aaf5988803ff966a58f2335a642a
godwinonenyi <godwinonenyi@gmail.com>
2023-09-12 16:12:28 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
 /**
  * main - Entry point
  *
@@ -10,7 +8,16 @@
 int main(void)
 {
 
-printf("_putchar\n");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 
 return (0);
 }
+
+
