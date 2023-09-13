04d92b60b480b6e299b0d5f07a832c197e5b194c
godwinonenyi <godwinonenyi@gmail.com>
2023-09-13 11:55:51 -0700

    times table

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+int sum = 0;
+int i;
+
+for (i = 0; i < 1024; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+sum = sum + i;
+}
+printf("%d\n", sum);
+return (0);
+}
