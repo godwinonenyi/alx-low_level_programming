efc20a2f08c751f5488ccdfaf06ce75d4b162a37
godwinonenyi <godwinonenyi@gmail.com>
2023-09-14 16:54:07 -0700

    comment on while

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * @n :varable to be used
+ * Return: no return
+ */
+void print_numbers(void)
+{
+
+int x;
+
+for (x = 48; x <= 57; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+}
