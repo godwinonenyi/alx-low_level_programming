769924ceae84250346d492419388f6accbeeb35d
godwinonenyi <godwinonenyi@gmail.com>
2023-09-15 03:36:22 -0700

    lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - prints straight line
+ * @n: number of times to print the character '_'
+ * Return: void
+ */
+void print_line(int n)
+{
+if (n <= 0)
+
+_putchar('\n');
+else
+{
+
+int j;
+
+for (j = 0; j <= n; j++)
+_putchar('_');
+_putchar('\n');
+}
+}
