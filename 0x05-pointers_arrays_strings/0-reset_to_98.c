0f4785e518115b709fad772c327cb00393d9bf41
godwinonenyi <godwinonenyi@gmail.com>
2023-09-18 05:42:51 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an integer as parameter
+ * updates the valueit point to to 98
+ * @n: pointer to the variable to update
+ *
+ */
+
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+This task is about pointers.
+What pointers are,how to use them.
+What are the differences between pointers and arrays.
+How to use strings and how to manipulate them.
+And finnally what is the scope of a variable.
