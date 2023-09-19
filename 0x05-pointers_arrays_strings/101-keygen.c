3a1fd412e525dec8a8359b5faf59b4e6e389b3a5
godwinonenyi <godwinonenyi@gmail.com>
2023-09-18 21:24:27 -0700

    keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random valid passwords for the program 101-crackme
+ * 
+ * Return: 0
+ */
+
+int main(void)
+{
+	int sum;
+	char c;
+
+	srand(time(NULL));
+	sum = 0;
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+
+	return (0);
+}
