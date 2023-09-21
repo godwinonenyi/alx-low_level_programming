75bcd00bb8bca45c53e835ba21384a7e72ee46cf
godwinonenyi <godwinonenyi@gmail.com>
2023-09-21 16:17:47 -0700

    copying of strinf

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *_strncpy - Copies up to n characters from src to dest.
+ * @dest: The destination string.
+ * @*src: The source string.
+ * @n: The maximum number of characters to copy from src.
+ *
+ * Return: A pointer to the destination string.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
