dec2b3c68ed9c2da220c813e267024ca3bf5d371
godwinonenyi <godwinonenyi@gmail.com>
2023-09-21 20:29:52 -0700

    moozzar

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * leet - a function that encodes a string into 1337
+ * @str: set of strings to be encoded
+ *
+ * Return: Encoded string
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char c[] = "aAeEoOtTlL";
+	char d[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; c[j] != '\0'; j++)
+		{
+			if (str[i] == c[j])
+			{
+				str[i] = d[j];
+			}
+		}
+	}
+	return (str);
+}
