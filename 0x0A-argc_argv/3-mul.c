835bc4669ba81a9cdf4bc84e3fa583e22082ff02
godwinonenyi <godwinonenyi@gmail.com>
2023-10-03 04:10:44 -0700

    multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers
+ * @argc: number of command line arguement
+ * @argv: the array that contains the command line arguement
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+	printf("Error\n");
+	return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
