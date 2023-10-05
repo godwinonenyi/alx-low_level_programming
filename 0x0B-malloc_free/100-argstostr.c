876c694165bcbe1b431c421abd76909cd0590c54
godwinonenyi <godwinonenyi@gmail.com>
2023-10-05 11:31:51 -0700

    argstostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,38 +1,43 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * argstostr - function that concatenates all the arguments of my program
+ * argstostr - concatenate all program arguments
+ *
  * @ac: argument count
  * @av: argument vector
+ *
  * Return: pointer to a new string, or NULL if it fails
  */
+
 char *argstostr(int ac, char **av)
 {
-	int i, n, k = 0, l = 0;
 	char *aout;
+	int i, n, k = 0, ln = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 	for (i = 0; i < ac; i++)
 	{
-	for (n = 0; av[i][n]; n++)
-		l++;
+		for (n = 0; av[i][n]; n++)
+			ln++;
 	}
-	l += ac;
-	aout = malloc(sizeof(char) * l + 1);
+	ln += ac;
+
+	aout = malloc(sizeof(char) * ln + 1);
 	if (aout == NULL)
 		return (NULL);
+
 	for (i = 0; i < ac; i++)
 	{
-	for (k = 0; av[i][n]; n++)
-	{
-		aout[k] = av[i][n];
-		k++;
-	}
-	if (aout[k] == '\0')
-	{
-		aout[k++] = '\n';
-	}
+		for (n = 0; av[i][n]; n++)
+		{
+			aout[k] = av[i][n];
+			k++;
+		}
+		if (aout[k] == '\0')
+		{
+			aout[k++] = '\n';
+		}
 	}
 	return (aout);
 }
