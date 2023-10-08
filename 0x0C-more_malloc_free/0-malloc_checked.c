682a7948418fd22b3ec6f973ad0edb9f30cd7ae5
godwinonenyi <godwinonenyi@gmail.com>
2023-10-08 00:54:04 -0700

    malloc check

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
 * malloc_checked - allocates memory using malloc
@@ -14,5 +15,5 @@ void *malloc_checked(unsigned int b)
 	j = malloc(b);
 	if (j == NULL)
 	exit(98);
-	return (i);
+	return (j);
 }
