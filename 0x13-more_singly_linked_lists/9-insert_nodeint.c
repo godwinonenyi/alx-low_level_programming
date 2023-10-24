97ba1bfcff13b1906a6e8ac9f5b8bc1f60834f32
godwinonenyi <godwinonenyi@gmail.com>
2023-10-24 16:34:04 -0700

    insert nodeint

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,7 +10,7 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new = malloc(sizeof(listint_t))
+	listint_t *new = malloc(sizeof(listint_t));
 	listint_t *temp = *head;
 	unsigned int i;
 
