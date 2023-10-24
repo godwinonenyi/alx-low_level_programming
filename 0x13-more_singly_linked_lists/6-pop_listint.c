d3ebddbce9f5787ddef75f18c8afc1cc8819ac9c
godwinonenyi <godwinonenyi@gmail.com>
2023-10-24 16:41:37 -0700

    pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -2,7 +2,7 @@
 
 /**
  * pop_listint - deletes the head node of a listint_t list
- * @head: pointer to the first element of the list
+ * @head: pointer to a ponter to a linked list lisint_t
  *
  * Return: the head node's data or 0 if the list is empty
  */
@@ -15,12 +15,8 @@ int pop_listint(listint_t **head)
 	if (!head || !*head)
 	return (0);
 
-	while (temp->next)
-	{
-	temp = temp->next;
-	}
-
 	num = (*head)->n;
+	temp = (*head)->next;
 	free(*head);
 	*head = NULL;
 	*head = temp;
