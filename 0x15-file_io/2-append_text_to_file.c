b7e3acc5d73fc3bb16b5aa7f9b98310d999985de
godwinonenyi <godwinonenyi@gmail.com>
2023-10-30 20:07:08 -0700

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * append_text_to_file - function that appends text at the 
- * end of a file.  
- * @filename: filename - is the name of the file and 
+ * append_text_to_file - function that appends text at the
+ * end of a file
+ * @filename: is the name of the file and
  * text_content is the NULL
  * @text_content: added content.
  *
